32b628a96516f229d21dd1a5fb4c1cadf13d1e75
Jacob Kwitkoski <jacobkwitkoski@gmail.com>
2013-03-27 16:48:28 -0700

    Initial commit, added basic ExampleExtension with some strings changed.

---
+++ gtalk-extension/src/ca/jacobk/dashclockgtalkextension/GTalkExtension.java
@@ -0,0 +1,31 @@
+package ca.jacobk.dashclockgtalkextension;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+
+public class GTalkExtension extends DashClockExtension {
+	private static final String TAG = "GTalkExtension";
+
+    public static final String PREF_NAME = "pref_name";
+	@Override
+	protected void onUpdateData(int arg0) {
+		// Get preference value.
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = sp.getString(PREF_NAME, getString(R.string.pref_name_default));
+
+        // Publish the extension data update.
+        publishUpdate(new ExtensionData()
+                .visible(true)
+                .icon(R.drawable.ic_launcher)
+                .status("Hello")
+                .expandedTitle("Hello, " + name + "!")
+                .expandedBody("Thanks for checking out this GTalk extension for DashClock.")
+                .clickIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))));
+	}
+
+}
