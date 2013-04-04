29867511ba1e3c64eed28d0ddc43dba2b8e17a67
Chris Wood <chriswood.ca@gmail.com>
2013-04-04 15:47:57 -0700

    Code for reading gtalk messages
    Permission READ_ONLY set for accessing gtalk messages.
    Intial code for parsing through messages from content URI.
    There has to be a different way to do this using Googles own content
    provider or broadcast intent as there doesnt look to be the correct
    information provided in in
    content://com.google.android.providers.talk/messages such as sender or
    message_status ie. cannot tell who mesage is from or if it is read or
    unread

--- gtalk-extension/src/ca/jacobk/dashclockgtalkextension/GTalkExtension.java
+++ gtalk-extension/src/ca/jacobk/dashclockgtalkextension/GTalkExtension.java
@@ -3,13 +3,18 @@ package ca.jacobk.dashclockgtalkextension;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class GTalkExtension extends DashClockExtension {
 	private static final String TAG = "GTalkExtension";
+	private static final Uri CONTENT_URI =
+			Uri.parse("content://com.google.android.providers.talk/messages");
 
     public static final String PREF_NAME = "pref_name";
 	@Override
@@ -18,8 +23,52 @@ public class GTalkExtension extends DashClockExtension {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         String name = sp.getString(PREF_NAME, getString(R.string.pref_name_default));
 
+        Log.w("chirs", CONTENT_URI.getAuthority());
+        
+        Cursor cur = null;
+        ContentResolver cr = getContentResolver();
+        
+        //edit to only get needed columns back .... nicknames, body only where message = unread
+        cur = cr.query(CONTENT_URI, null, null, null, null);
+        
+        Log.w("chris", "col: " + cur.getColumnCount());
+        String names[] = cur.getColumnNames();
+        for(int i = 0; i < cur.getColumnCount(); i++)
+        {
+        	Log.w("chris", names[i]);
+        }
+        Log.w("chris", "row: " + cur.getCount());
+        
+        String r = "";
+
+        cur.moveToFirst();
+        for (int i = 0; i < 15; i++)
+        {
+        	r += cur.getString(i) + " ";
+        }
+        Log.w("message", r);
+        r = "";
+        
+        cur.moveToNext();
+        for (int i = 0; i < 15; i++)
+        {
+        	r += cur.getString(i) + " ";
+        }
+        Log.w("message", r);
+        r = "";
+        
+        cur.moveToLast();
+        for (int i = 0; i < 15; i++)
+        {
+        	r += cur.getString(i) + " ";
+        }
+        Log.w("message", r);
+        r = "";
+        
+        
+        
         // Publish the extension data update.
-        publishUpdate(new GTalkData("Hello", "Hello" + name, 
+        publishUpdate(new GTalkData("Hello", "Hello " + name, 
         		"GTalk DashClock has begun...",
         		new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))));
         /*publishUpdate(new ExtensionData()
